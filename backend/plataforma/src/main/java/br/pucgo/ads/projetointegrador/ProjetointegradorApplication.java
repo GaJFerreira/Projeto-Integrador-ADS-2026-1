d49e109b0c978b47bcb3097bda9e2248d2945581
richardeveloper <richard_ar@hotmail.com>
2026-04-28 09:31:08 -0300

    Configurando aplicação em módulos

--- backend/plataforma/src/main/java/br/pucgo/ads/projetointegrador/ProjetointegradorApplication.java
+++ backend/plataforma/src/main/java/br/pucgo/ads/projetointegrador/ProjetointegradorApplication.java
@@ -2,7 +2,27 @@ package br.pucgo.ads.projetointegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = "br.pucgo.ads.projetointegrador")
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages =
+	{
+		"br.pucgo.ads.projetointegrador",
+		"br.com.puc.saborfamilia"
+	}
+)
+@EntityScan(basePackages =
+	{
+		"br.pucgo.ads.projetointegrador.plataforma.entity",
+ 		"br.com.puc.saborfamilia.database.entity"
+	}
+)
+@EnableJpaRepositories(basePackages =
+	{
+		"br.pucgo.ads.projetointegrador.plataforma.repository",
+		"br.com.puc.saborfamilia.database.repository"
+	}
+)
 public class ProjetointegradorApplication {
 
 	public static void main(String[] args) {
